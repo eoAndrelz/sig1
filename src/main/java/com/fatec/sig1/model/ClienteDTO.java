b929e354a69474dff6383e1b2dd66020ba3a2c8b
eoAndrelz <raq.andre101810@gmail.com>
2023-04-08 12:29:35 -0300

    Atualização de Projeto

---
+++ src/main/java/com/fatec/sig1/model/ClienteDTO.java
@@ -0,0 +1,92 @@
+package com.fatec.sig1.model;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import org.hibernate.validator.constraints.br.CPF;
+
+public class ClienteDTO {
+	@NotBlank(message = "Nome é requerido")
+	private String nome;
+	@Pattern(regexp="^(0?[1-9]|[12][0-9]|3[01])[\\/-](0?[1-9]|1[012])[\\/-]\\d{4}$", message="A data de nascimento deve estar no formato dd/MM/YYYY")
+	private String dataNascimento;
+	@Pattern(regexp="^[0-9]{2}-([0-9]{8}[0-9]{9}|)")
+	private String telefone;
+	@NotBlank(message = "Sexo M/F")
+	private String sexo;
+	@CPF
+	private String cpf;
+	@NotBlank(message = "O CEP é obrigatório.")
+	private String cep;
+	@NotBlank(message = "O complemento é obrigatório.")
+	private String complemento;
+
+	public ClienteDTO(String nome, String dataNascimento, String sexo, String cpf, String cep,String telefone, String complemento) {
+		this.nome = nome;
+		setDataNascimento(dataNascimento);
+		this.telefone=telefone;
+		this.sexo = sexo;
+		this.cpf = cpf;
+		this.cep = cep;
+		this.complemento = complemento;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(String dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public String getSexo() {
+		return sexo;
+	}
+
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+
+	public String getComplemento() {
+		return complemento;
+	}
+
+	public void setComplemento(String complemento) {
+		this.complemento = complemento;
+	}
+	
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public Cliente retornaUmCliente() {
+		return new Cliente(nome, dataNascimento,telefone,sexo, cpf, cep, complemento);
+	}
+}
